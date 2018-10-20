34e029fbccb972f37d69a7abb33bf53c2134527a
aprildenise <admdizonn@gmail.com>
2018-10-20 18:17:01 -0400

    added graph
    added a graph implementation of the first level/2d grid

--- AbstractMaze.cs
+++ AbstractMaze.cs
@@ -22,14 +22,22 @@ public class AbstractMaze : MonoBehaviour {
     //(NOTE: may be moved to another class; size of list is currently only 1. Need to expand to hold all sets of clusters)
     public List<Cluster> C;
 
+    public List<Node[,]> clusterNodes;
+
 
     //for testing
     private void Start()
     {
-        Vector2 dimensions = GetSizeOfMap();
+        GetSizeOfMap();
+
+        /*
         mapWidth = dimensions.x;
         mapHeight = dimensions.y;
         InitAbstractMaze(dimensions.x, dimensions.y, 5);
+        */
+
+        clusterNodes = new List<Node[,]>();
+        InitAbstractMaze(5);
     }
 
 
@@ -38,7 +46,7 @@ public class AbstractMaze : MonoBehaviour {
     /* Determine the entire size of the map using the corners of the map
      * Output: the width and the height of the map, stored in a Vector2
      */
-    private Vector2 GetSizeOfMap()
+    private void GetSizeOfMap()
     {
 
         //use the Renderers of the corners to find where they are in world coordinates
@@ -48,12 +56,9 @@ public class AbstractMaze : MonoBehaviour {
         float approxTop = (topleft.bounds.center.z + (topleft.bounds.size.z / 2));
 
         //use the world coordinates to get the width and the height
-        float approxWidth = Mathf.Round(Mathf.Abs(approxLeft - approxRight));
-        float approxHeight = Mathf.Round(Mathf.Abs(approxTop - approxBottom));
-
+        mapWidth = Mathf.Round(Mathf.Abs(approxLeft - approxRight));
+        mapHeight = Mathf.Round(Mathf.Abs(approxTop - approxBottom));
         mapTopLeft = new Vector3(approxLeft, topleft.bounds.center.y, approxTop);
-
-        return new Vector2(approxWidth, approxHeight);
 
     }
 
@@ -63,15 +68,20 @@ public class AbstractMaze : MonoBehaviour {
     /* Initialze the abstract maze
      * Input: width of the map, height of the map, the length of a SQUARE cluster
      */
-    public void InitAbstractMaze(float mapWidth, float mapHeight, float clusterSize) 
+    public void InitAbstractMaze(int clusterSize)
     {
+
+        /* Old code
         //calculate the size of a cluster based on the dimensions of a map
         float clusterHeight = mapHeight / clusterSize;
         float clusterWidth = mapWidth / clusterSize;
 
         //get the set of 1-clusters (the lowest possible level)
         C = BuildClusters(1, clusterWidth, clusterHeight);
-        
+        */
+
+        //get the set of the lowest level clusters (2D grid)
+        clusterNodes.Add(BuildClusterGraph(1, 5));
 
     }
 
@@ -97,6 +107,7 @@ public class AbstractMaze : MonoBehaviour {
                 for (int j = 0; j < (mapHeight / clusterHeight); j++)
                 {
                     Vector3 position = new Vector3(worldTopLeft.x + (clusterWidth * j), worldTopLeft.y, worldTopLeft.z - (clusterHeight * i));
+
                     Cluster cluster = new Cluster(clusterWidth, clusterHeight, position, level);
                     clusters.Add(cluster);
                     //Debug.Log("cluster added at:" + position);
@@ -105,6 +116,45 @@ public class AbstractMaze : MonoBehaviour {
         }
 
         return clusters; 
+    }
+
+
+    /* Graph implementation of BuildClusters
+     */
+    public Node[,] BuildClusterGraph(int level, int clusterSize)
+    {
+
+        //calculate the size of a cluster based on the dimensions of a map
+        int numXClusters = (int)Mathf.Round(mapHeight / clusterSize);
+        int numYClusters = (int)Mathf.Round(mapWidth / clusterSize);
+
+        Node[,] grid = new Node[numXClusters, numYClusters];
+
+        if (level == 1)
+        {
+            //Build a 2d grid of cluster nodes
+            for (int x = 0; x < numXClusters; x++)
+            {
+                for (int y = 0; y < numYClusters; y++)
+                {
+                    //find the position of this node
+                    Vector3 worldPoint = mapTopLeft + (Vector3.right * (x * (clusterSize + (clusterSize/2)))) + (Vector3.forward * (y * (clusterSize + (clusterSize/2))));
+
+                    grid[x, y] = new Node(worldPoint, x, y, 1, "");
+
+                    /* old code
+                    //see if we should place a wall there
+                    PlaceWalls(worldPoint);
+
+                    //find if this node is walkable, depending if there is an unwakabe object on it
+                    bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                    //create the node
+                    grid[x, y] = new Node(walkable, worldPoint, x, y);
+                    */
+                }
+            }
+        }
+        return grid;
     }
 
 
---
+++ Neighbor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Neighbor
+{
+
+    public int vertexNum;
+    public Neighbor next;
+
+    public Neighbor(int vertexNum, Neighbor next)
+    {
+        this.vertexNum = vertexNum;
+        this.next = next;
+    }
+
+}
